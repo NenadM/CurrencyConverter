1041d642461d56bb51cdc3a0888ae5dab58d1470
Nenad Markovic <nenomark@gmail.com>
2021-03-10 15:21:36 +0100

    Code refactorings

---
+++ CurrencyConverter/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using CurrencyConverter.Views;
+using CurrencyConverter.ViewModels;
+
+namespace CurrencyConverter
+{
+    public class Bootstrapper
+    {
+        public Window BootstrapApplication()
+        {
+            var currencyConverterViewModel = new CurrencyConverterViewModel();
+            var mainWindow = new MainView(currencyConverterViewModel);
+
+            return mainWindow;
+
+            //mainWindow.Show();
+        }
+    }
+}
---
+++ CurrencyConverter/Views/MainView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace CurrencyConverter.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView(object mainViewDataContext)
+        {            
+            InitializeComponent();
+            this.DataContext = mainViewDataContext;
+        }
+    }
+}
