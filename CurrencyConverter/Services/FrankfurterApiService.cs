81f6b3ed3c518840c1f3b6b49a274721102ac3a5
Nenad Markovic <nenomark@gmail.com>
2021-04-08 16:25:34 +0200

    Introduce HttpClient extensions

--- CurrencyConverter/Services/FrankfurterApiService.cs
+++ CurrencyConverter/Services/FrankfurterApiService.cs
@@ -1,50 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Text.Json;
+using System.Net.Http.Json;
 
 namespace CurrencyConverter.Services
 {
     public class FrankfurterApiService : IFrankfurterApiService
     {
+        private readonly HttpClient httpClient = new HttpClient();
+
         public async Task<IEnumerable<CurrencyDto>> GetCurrenciesAsync()
         {
-            var client = new HttpClient();
-             
-            var body = await this.CallWebServiceAsync("https://api.frankfurter.app/currencies");
-
-            var result = JsonSerializer.Deserialize<Dictionary<string, string>>(body)
-                .Select(kvp => new CurrencyDto(kvp.Key, kvp.Value));
-
-            return result;
+            var result = await this.httpClient.GetFromJsonAsync<Dictionary<string, string>>("https://api.frankfurter.app/currencies");
+            return result.Select(kvp => new CurrencyDto(kvp.Key, kvp.Value));
         }
 
         public async Task<CurrencyConversionDto> ConvertAsync(string amount, string from, string to)
         {
-            var uri = $"https://api.frankfurter.app/latest?from={from}&to={to}&amount={amount}";
-            var body = await this.CallWebServiceAsync(uri);
-            var result = JsonSerializer.Deserialize<CurrencyConversionDto>(body);
-
-            return result;
-        }
-
-        private async Task<string> CallWebServiceAsync(string uri)
-        {
-            var client = new HttpClient();
-            var request = new HttpRequestMessage
-            {
-                Method = HttpMethod.Get,
-                RequestUri = new Uri(uri),
-            };
-            using (var response = await client.SendAsync(request))
-            {
-                response.EnsureSuccessStatusCode();
-                var body = await response.Content.ReadAsStringAsync();
-
-                return body;
-            }
+            return await this.httpClient.GetFromJsonAsync<CurrencyConversionDto>($"https://api.frankfurter.app/latest?from={from}&to={to}&amount={amount}");
         }
     }
 }
